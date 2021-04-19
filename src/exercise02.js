8ec90514e3a63d31fc60b2a5073d50842a92aaa2
KallerMG <50585470+KallerMG@users.noreply.github.com>
2021-04-19 20:22:06 -0300

    e3

--- src/exercise02.js
+++ src/exercise02.js
@@ -3,6 +3,8 @@
   substitua todas as ocorrências de números por `$`.
 */
 
+const trocaOcorrencia = (string) => string.replace(/[\d]/g, "$");
+
 window.exercise02 = function () {
-  console.log("resultado ex2: ");
+  console.log("resultado ex2: ", trocaOcorrencia("teste 1 de 2 string 3"));
 };
--- src/exercise03.js
+++ src/exercise03.js
@@ -3,6 +3,15 @@
   substitua os números na frase `‘T35t3 d3 35t4g1o’` conforme a sua respectiva letra
 */
 
-window.exercise03 = function() {
-  console.log("resultado ex3: ");
+let objetos = { 4: "a", 3: "e", 1: "i", 5: "s" };
+
+const trocaObjeto = (string, objetos) => {
+  for (let prop in objetos) {
+    string = string.replace(new RegExp(prop, "g"), String(objetos[prop]));
+  }
+  return string;
+};
+
+window.exercise03 = function () {
+  console.log("resultado ex3: ", trocaObjeto("T35t3 d3 35t4g1o", objetos));
 };
